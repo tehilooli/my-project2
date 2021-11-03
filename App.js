bdb88d7ba3444bf54110e0e487d4ec0d093da808
Tehila Dahan <33831931+tehilooli@users.noreply.github.com>
2021-11-03 20:56:06 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HomePage from './comps/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
